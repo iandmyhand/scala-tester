a0a0dfb86ca17062845e7eb2138ff8ebef226c70
SeomGi, Han <iandmyhand@gmail.com>
2016-10-12 19:28:35 +0900

    Update a generics tester.

--- src/hackerrank/SolutionGenerics.scala
+++ src/hackerrank/SolutionGenerics.scala
@@ -1,9 +1,21 @@
+class Generics {
+    def printArray[T](arr: Array[T]): Unit = {
+        for(n <- 0 until arr.length) {
+            println(arr(n).toString)
+        }
+    }
+}
+
 object SolutionGenerics {
     def main(args: Array[String]): Unit = {
         val is = Array(1, 2, 3)
         val ss = Array("A", "B", "C")
         printArray(is)
         printArray(ss)
+
+        val g = new Generics
+        g.printArray(is)
+        g.printArray(ss)
     }
 
     def printArray(arr: Array[_]): Unit = {
